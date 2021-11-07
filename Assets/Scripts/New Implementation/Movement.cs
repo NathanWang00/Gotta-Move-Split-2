c581cdf502c74377deb271948ba263f8ed861033
samhvik <samhvik@gmail.com>
2021-11-06 17:01:41 -0700

    Removed variable wall jump

--- Assets/Scripts/New Implementation/Movement.cs
+++ Assets/Scripts/New Implementation/Movement.cs
@@ -305,7 +305,7 @@ public class Movement : MonoBehaviour
 
         Vector2 wallDir = coll.onRightWall ? Vector2.left : Vector2.right;
 
-        Jump((Vector2.up / 1.5f + wallDir * Mathf.Abs(Input.GetAxis("Horizontal"))), true);
+        Jump((Vector2.up / 1.5f + wallDir / 1.5f), true);
 
         wallJumped = true;
     }
